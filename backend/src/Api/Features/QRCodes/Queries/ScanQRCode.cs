831aa6f8a8a62b7ced6a5e4ae140f4744270e7bc
Marek Smogrovic <smogrovic.marek@gmail.com>
2025-10-19 23:16:17 +0200

    feat: add qr code scan endpoint

---
+++ backend/src/Api/Features/QRCodes/Queries/ScanQRCode.cs
@@ -0,0 +1,70 @@
+using System.Security.Claims;
+using Carter;
+using Fei.Is.Api.Common.Errors;
+using Fei.Is.Api.Data.Contexts;
+using Fei.Is.Api.Data.Models;
+using Fei.Is.Api.Features.QRCodes.Extensions;
+using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fei.Is.Api.Features.QRCodes.Queries;
+
+public static class ScanQRCode
+{
+    public sealed class Endpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet(
+                    "qr-codes/{shortCode:string}/scan",
+                    async Task<Results<Ok<Response>, NotFound, ForbidHttpResult>> (IMediator mediator, string shortCode) =>
+                    {
+                        var query = new Query(shortCode);
+                        var result = await mediator.Send(query);
+
+                        if (result.HasError<NotFoundError>())
+                        {
+                            return TypedResults.NotFound();
+                        }
+                        if (result.HasError<ForbiddenError>())
+                        {
+                            return TypedResults.Forbid();
+                        }
+
+                        return TypedResults.Ok(result.Value);
+                    }
+                )
+                .WithName(nameof(ScanQRCode))
+                .WithTags(nameof(QRCode))
+                .AllowAnonymous()
+                .WithOpenApi(o =>
+                {
+                    o.Summary = "Get a QR code by short code";
+                    return o;
+                });
+        }
+    }
+
+    public record Query(string ShortCode) : IRequest<Result<Response>>;
+
+    public sealed class Handler(AppDbContext context) : IRequestHandler<Query, Result<Response>>
+    {
+        public async Task<Result<Response>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var qrCode = await context.QRCodes.AsNoTracking().FirstOrDefaultAsync(qrCode => qrCode.ShortCode == request.ShortCode, cancellationToken);
+
+            if (qrCode == null)
+            {
+                return Result.Fail(new NotFoundError());
+            }
+
+            var response = new Response(qrCode.RedirectUrl);
+
+            return Result.Ok(response);
+        }
+    }
+
+    public record Response(string RedirectUrl);
+}
