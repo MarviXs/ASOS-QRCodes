cda43b41eabd6fffb14b2295e17ef4d50ad99f91
Marek Smogrovic <smogrovic.marek@gmail.com>
2025-10-20 15:02:19 +0200

    feat: DeviceType migration

---
+++ backend/src/Api/Data/Migrations/20251020130200_QRScanDeviceType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fei.Is.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class QRScanDeviceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DeviceType",
+                table: "ScanRecords",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DeviceType",
+                table: "ScanRecords",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
